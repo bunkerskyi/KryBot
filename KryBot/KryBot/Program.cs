cf59ed120878be5e9c0c029c6d0305ac24c187a9
Колодривский Иван Николаевич <kribetko@gmail.com>
2016-03-03 23:07:58 +0200

    CHANGE: Change force emulation IE10 to IE11 Изменена принудительная эмуляция IE10 на IE11

--- KryBot/KryBot/Program.cs
+++ KryBot/KryBot/Program.cs
@@ -16,7 +16,7 @@ namespace KryBot
             Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION");
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
             string programName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
-            key?.SetValue(programName, 10001, RegistryValueKind.DWord);
+            key?.SetValue(programName, (decimal)11000, RegistryValueKind.DWord);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
