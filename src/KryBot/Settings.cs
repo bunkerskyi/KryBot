687d5c6ea7393b130401437a41fa5089267ddc92
KruBet <kribetko@gmail.com>
2016-05-10 14:55:49 +0300

    FIX: Fixed settings save

--- src/KryBot/Settings.cs
+++ src/KryBot/Settings.cs
@@ -7,19 +7,19 @@ namespace KryBot
 {
     public class Settings
     {
-        private bool LogActive { get; set; }
-        private string Lang { get; set; }
-        private bool Sort { get; set; }
-        private bool SortToMore { get; set; }
-        private bool SortToLess { get; set; }
-        private bool Timer { get; set; }
-        private bool Autorun { get; set; }
-        private bool ShowWonTip { get; set; }
-        private bool ShowFarmTip { get; set; }
-        private bool FullLog { get; set; }
-        private bool WishlistSort { get; set; }
-        private int TimerInterval { get; set; }
-        private int TimerLoops { get; set; }
+        public bool LogActive { get; set; }
+        public string Lang { get; set; }
+        public bool Sort { get; set; }
+        public bool SortToMore { get; set; }
+        public bool SortToLess { get; set; }
+        public bool Timer { get; set; }
+        public bool Autorun { get; set; }
+        public bool ShowWonTip { get; set; }
+        public bool ShowFarmTip { get; set; }
+        public bool FullLog { get; set; }
+        public bool WishlistSort { get; set; }
+        public int TimerInterval { get; set; }
+        public int TimerLoops { get; set; }
 
         public void Load()
         {
